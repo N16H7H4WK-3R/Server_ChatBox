57ecfa893e4d3f303ac2e556328ad1846f341f43
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-06-21 07:21:42 +0530

    Production Updates

--- client.js
+++ client.js
@@ -1,53 +1,80 @@
 let currentUser = null;
-const ws = new WebSocket("ws://64.227.156.150:8765");
+const websocketURL = "ws://64.227.156.150:8765";
+let ws;
 
-ws.onopen = () => {
-    console.log("Connected to WebSocket server");
-};
+function connectWebSocket() {
+    ws = new WebSocket(websocketURL);
+
+    ws.onopen = () => {
+        showToast("Server Connection Successful", "success");
+    };
+
+    ws.onmessage = function (event) {
+        handleMessage(event);
+    };
+
+    ws.onerror = function (event) {
+        showToast("WebSocket error observed", "danger");
+    };
+
+    ws.onclose = function () {
+        showToast("WebSocket connection closed. Reconnecting...", "warning");
+        setTimeout(connectWebSocket, 5000); // Try to reconnect after 5 seconds
+    };
+}
+
+function handleMessage(event) {
+    const data = JSON.parse(event.data);
+
+    if (data.type === "new_message") {
+        const messageItem = createMessageItem(data.message, data.sender, false);
+        appendMessageToUI(messageItem);
+    } else if (data.type === "chat_history") {
+        const chatHistory = data.chat_history;
+        const messageList = document.querySelector(".list-unstyled");
+        chatHistory.reverse().forEach((msg) => {
+            const messageItem = createMessageItem(msg.message, msg.remote_address, false);
+            messageList.appendChild(messageItem);
+        });
+    } else if (data.type === "time_interval_messages") {
+        const messages = data.messages;
+        const messageList = document.querySelector(".list-unstyled");
+        messageList.innerHTML = ""; // Clear current messages
+        messages.forEach((msg) => {
+            const messageItem = createMessageItem(msg.message, msg.remote_address, false);
+            messageList.appendChild(messageItem);
+        });
+    } else if (data.type === "total_clients") {
+        updateTotalClients(data.total_clients);
+    }
+}
 
 function createMessageItem(content, sender, isSelf) {
     const li = document.createElement("li");
-    li.classList.add("mb-2");
-    li.classList.add("border");
-    li.classList.add("rounded");
-    li.classList.add("p-2");
+    li.classList.add("mb-2", "border", "rounded", "p-2");
 
     const div = document.createElement("div");
-    div.classList.add("d-flex");
-    div.classList.add(isSelf ? "justify-content-end" : "justify-content-start");
-    div.classList.add("align-items-center");
+    div.classList.add("d-flex", isSelf ? "justify-content-end" : "justify-content-start", "align-items-center");
 
     const messageP = document.createElement("p");
     messageP.textContent = content;
     messageP.classList.add("m-0");
 
+    const img = document.createElement("img");
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR1afMYs4XDwG9x7WeYWKNBQLZMBAWBS1ekA&s";
+    img.alt = "Avatar";
+    img.classList.add("rounded-circle", isSelf ? "ms-3" : "me-3", "image_style");
+
+    const nameP = document.createElement("p");
+    nameP.textContent = sender;
+    nameP.classList.add("small", "text-muted", "m-1", isSelf ? "me-auto" : "ms-auto");
+    nameP.style.fontSize = "0.6rem";
+
     if (isSelf) {
-        const img = document.createElement("img");
-        img.src =
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR1afMYs4XDwG9x7WeYWKNBQLZMBAWBS1ekA&s";
-        img.alt = "Avatar";
-        img.classList.add("rounded-circle");
-        img.classList.add("ms-3");
-        img.classList.add("image_style");
-        const nameP = document.createElement("p");
-        nameP.textContent = sender;
-        nameP.classList.add("small", "text-muted", "m-1", "me-auto");
-        nameP.style.fontSize = "0.6rem";
         div.appendChild(nameP);
         div.appendChild(messageP);
         div.appendChild(img);
     } else {
-        const img = document.createElement("img");
-        img.src =
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR1afMYs4XDwG9x7WeYWKNBQLZMBAWBS1ekA&s";
-        img.alt = "Avatar";
-        img.classList.add("rounded-circle");
-        img.classList.add("me-3");
-        img.classList.add("image_style");
-        const nameP = document.createElement("p");
-        nameP.textContent = sender;
-        nameP.classList.add("small", "text-muted", "m-1", "ms-auto");
-        nameP.style.fontSize = "0.6rem";
         div.appendChild(img);
         div.appendChild(messageP);
         div.appendChild(nameP);
@@ -57,7 +84,6 @@ function createMessageItem(content, sender, isSelf) {
     return li;
 }
 
-// Function to send a new message
 function sendMessage() {
     const messageInput = document.getElementById("message-input");
 
@@ -69,66 +95,17 @@ function sendMessage() {
     const sender = "You";
     const messageItem = createMessageItem(messageContent, sender, true);
 
-    // Append message to the UI
-    const messageList = document.querySelector(".list-unstyled");
-    messageList.appendChild(messageItem);
+    appendMessageToUI(messageItem);
 
-    // Send message to the WebSocket server
     ws.send(JSON.stringify({ type: "new_message", message: messageContent }));
 
     messageInput.value = "";
 }
 
-// Handle incoming messages from the WebSocket server
-ws.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-
-    if (data.type === "new_message") {
-        const messageContent = {
-            message: data.message,
-            sender: data.sender,
-        };
-        const messageItem = createMessageItem(
-            messageContent.message,
-            messageContent.sender,
-            false
-        );
-
-        // Append incoming message to the UI
-        const messageList = document.querySelector(".list-unstyled");
-        messageList.appendChild(messageItem);
-    } else if (data.type === "chat_history") {
-        const chatHistory = data.chat_history;
-        const messageList = document.querySelector(".list-unstyled");
-        chatHistory.reverse().forEach((msg) => {
-            const messageItem = createMessageItem(
-                msg.message,
-                msg.remote_address,
-                false
-            );
-            messageList.appendChild(messageItem);
-        });
-    } else if (data.type === "time_interval_messages") {
-        const messages = data.messages;
-        const messageList = document.querySelector(".list-unstyled");
-        messageList.innerHTML = ""; // Clear current messages
-        messages.forEach((msg) => {
-            const messageItem = createMessageItem(
-                msg.message,
-                msg.remote_address,
-                false
-            );
-            messageList.appendChild(messageItem);
-        });
-    } else if (data.type === "total_clients") {
-        updateTotalClients(data.total_clients);
-    }
-};
-
-// Handle WebSocket connection errors
-ws.onerror = function (event) {
-    console.error("WebSocket error observed:", event);
-};
+function appendMessageToUI(messageItem) {
+    const messageList = document.querySelector(".list-unstyled");
+    messageList.appendChild(messageItem);
+}
 
 function updateTotalClients(total) {
     const totalClientsElement = document.getElementById("totalClients");
@@ -140,15 +117,44 @@ function getMessagesByTimeInterval() {
     const endTime = document.getElementById("end-time").value;
 
     if (!startTime || !endTime) {
-        alert("Please select both start and end time.");
+        showToast("Please select both start and end time.", "warning");
         return;
     }
 
-    ws.send(
-        JSON.stringify({
-            type: "get_messages_by_time_interval",
-            start_time: startTime,
-            end_time: endTime,
-        })
-    );
+    ws.send(JSON.stringify({
+        type: "get_messages_by_time_interval",
+        start_time: startTime,
+        end_time: endTime,
+    }));
 }
+
+function showToast(message, type) {
+    const toastContainer = document.getElementById('toastContainer');
+    const toast = document.createElement('div');
+    toast.className = `toast align-items-center text-bg-${type} border-0`;
+    toast.role = 'alert';
+    toast.ariaLive = 'assertive';
+    toast.ariaAtomic = 'true';
+
+    const toastContent = `
+        <div class="d-flex">
+            <div class="toast-body">
+                ${message}
+            </div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    `;
+
+    toast.innerHTML = toastContent;
+    toastContainer.appendChild(toast);
+
+    const bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+
+    toast.addEventListener('hidden.bs.toast', () => {
+        toast.remove();
+    });
+}
+
+// Initialize WebSocket connection
+connectWebSocket();
